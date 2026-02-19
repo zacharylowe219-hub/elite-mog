a6be21a56abd873ab7691a86deda51769d4daaf3
LooksMax <zacharylowe219@gmail.com>
2026-02-19 15:31:13 -0500

    Update checkout.js

--- api/apiapi/checkout.js
+++ api/apiapi/checkout.js
@@ -1,8 +1,10 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 export default async function handler(req, res) {
+
+try {
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const session = await stripe.checkout.sessions.create({
 
@@ -13,7 +15,7 @@ line_items: [
 price_data: {
 currency: "usd",
 product_data: {
-name: "ELITE MOG Consulting Session"
+name: "ELITE MOG Session"
 },
 unit_amount: 3000
 },
@@ -23,12 +25,18 @@ quantity: 1
 
 mode: "payment",
 
-success_url: "https://yourdomain.vercel.app",
-
-cancel_url: "https://yourdomain.vercel.app",
+success_url: "https://elite-mog.vercel.app",
+cancel_url: "https://elite-mog.vercel.app",
 
 });
 
-res.json({ id: session.id });
+res.status(200).json({ id: session.id });
+
+} catch (error) {
+
+res.status(500).json({ error: error.message });
 
 }
+
+}
+
